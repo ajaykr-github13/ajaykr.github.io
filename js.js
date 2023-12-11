1eb9d9e64a00b008f78b93b7fcfacb7846ce6613
Ajay kumar <153365595+ajaykr-github13@users.noreply.github.com>
2023-12-11 14:53:03 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+// <!-- <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script> -->
+
+    
+        var typed = new Typed('#element', {
+          strings: ['programmer', 'Designer','gammer'],
+          typeSpeed: 50,
+          backSpeed:50,
+          loop:true
+    
+        });
+     
